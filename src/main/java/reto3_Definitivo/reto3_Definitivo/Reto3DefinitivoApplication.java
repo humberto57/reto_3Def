eb0877dfb55777502dedf5144da2cf2b2fd116c5
unknown <humberto.barbosa.mt@correo.usa.edu.co>
2021-11-01 16:36:00 -0500

    reto_3def

---
+++ src/main/java/reto3_Definitivo/reto3_Definitivo/Reto3DefinitivoApplication.java
@@ -0,0 +1,13 @@
+package reto3_Definitivo.reto3_Definitivo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Reto3DefinitivoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Reto3DefinitivoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/reto3_Definitivo/reto3_Definitivo/Reto3DefinitivoApplicationTests.java
@@ -0,0 +1,13 @@
+package reto3_Definitivo.reto3_Definitivo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Reto3DefinitivoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
